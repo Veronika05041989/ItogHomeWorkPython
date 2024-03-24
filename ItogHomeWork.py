e92587f0a11e2e48c10477c39b02ae80d39b57df
Veronika <veronikarony@yandex.ru>
2024-03-24 10:22:47 +0300

    Дабавили код save и read note

--- ItogHomeWork.py
+++ ItogHomeWork.py
@@ -23,3 +23,26 @@ def create_note():
         return None
 
     return note
+
+def save_notes(notes):
+    try:
+        with open(NOTES_FILE, 'w') as file:
+            json.dump(notes, file, indent=4)
+    except Exception as e:
+        print("An error occurred while saving notes:", e)
+        
+def read_notes():
+    try:
+        if not os.path.exists(NOTES_FILE):
+            print("No notes found.")
+            return
+        with open(NOTES_FILE) as file:
+            notes = json.load(file)
+            for note in notes:
+                print("ID: ", note["id"])
+                print("Title: ", note["title"])
+                print("Body: ", note["body"])
+                print("Created on: ", note["created_date"])
+                print()
+    except Exception as e:
+        print("An error occurred while reading notes:", e)
